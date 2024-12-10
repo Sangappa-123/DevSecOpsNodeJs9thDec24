db38a1dc2248cbcd8450689560584c471abab1bb
Sangappa-123 <birajsd@gmail.com>
2024-12-10 11:27:41 +0530

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,18 @@
 const request = require('supertest');
 const app = require('../app');
 
+let server; // Reference to the server
+
+beforeAll(() => {
+  // Start the server before tests
+  server = app.listen(3000);
+});
+
+afterAll(() => {
+  // Close the server after tests
+  server.close();
+});
+
 describe('GET /', () => {
   it('should respond with "Hello, GitHub Actions!"', async () => {
     const res = await request(app).get('/');
